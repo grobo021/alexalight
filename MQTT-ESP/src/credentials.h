b188a77cb12300b1a2417d9621f857a1acac7fe8
Rishab <rishabarora2008@Gmail.com>
2020-04-12 16:58:26 +0530

    add credentials.h

---
+++ MQTT-ESP/src/credentials.h
@@ -0,0 +1,7 @@
+const char *ssid = "*****";
+const char *password = "*****";
+
+const char *mqttServer = "maqiatto.com";
+const int mqttPort = 1883;
+const char *mqttUser = "*****";
+const char *mqttPassword = "*****";
